0461cbe85dceae57227f0eb42f11c3e742fed42a
PlusCaptcha <PlusCaptcha@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-21 19:34:57 +0000

    Fixed some things
    git-svn-id: https://plugins.svn.wordpress.org/pluscaptcha/tags/2.0.2@936229 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ PlusCaptcha.php
@@ -0,0 +1,153 @@
+<?php
+/*
+Plugin Name: PlusCaptcha
+Plugin URI: http://www.pluscaptcha.com/
+Description: PlusCaptcha - No more Spam, Easy Set-Up, you can Personalise it. Plus it's FREE!
+Version: 2.0.2
+Author: PlusCaptcha
+Author URI: http://www.pluscaptcha.com/
+License: GNU GPL2
+*/
+/*
+Copyright (C) 2014 IT Oeste. (www.pluscaptcha.com). All rights reserved.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+/*
+	For Irina Luchinetz & Igor Borovskoy. I'm not sure about if I could finish this project without their support. My infinite thanks to them! Thanks for make this dream possible, I know about you couldn't help to me like you would like, but this untidy house with infinite love is all I need, all everyone needs...
+		- Denis, CEO PlusCaptcha
+*/
+
+/* Ocultar errores */
+require_once(ABSPATH . 'wp-settings.php');
+error_reporting(0);
+/* *************** */
+
+define('PlusCaptcha_SITE_URL', 'http://www.pluscaptcha.com/');
+// for backward compatibility - 2.0
+defined('WP_PLUGIN_DIR') or define('WP_PLUGIN_DIR', ABSPATH . '/wp-content/plugins');
+
+// define absolute path to plugin
+define('PlusCaptcha_DIR_NAME', basename( dirname(__FILE__) ));
+define('PlusCaptcha_ROOT', WP_PLUGIN_DIR . '/' . PlusCaptcha_DIR_NAME);
+define('PlusCaptcha_URL', WP_PLUGIN_URL . '/' . PlusCaptcha_DIR_NAME);
+// define absolute path to plugin  PlusCaptcha.php
+define('PlusCaptcha_PHP_PATH', PlusCaptcha_URL . '/library/PlusCaptcha.php');
+// define absolute path to plugin library
+define('PlusCaptcha_LIBRARY', PlusCaptcha_ROOT . '/library');
+// define absolute path to plugin templates
+define('PlusCaptcha_TEMPLATE', PlusCaptcha_ROOT . '/template');
+define('PlusCaptcha_ERROR_MESSAGE', 'The solution of task you submitted was incorrect. Please read the instruction and try again.');
+define('PlusCaptcha_ERROR_MESSAGE_BR', 'The solution of task you submitted was incorrect.<br>Please read the instruction and try again.');
+
+
+/*
+********************** External Plugin Used ***********************
+*/
+
+	/*
+	 Thanks to Sweetcaptcha project to help to us with EcoPlus! Fight for the nature!
+	 Basecode Was Used.
+	*/
+	/*
+	 Thanks to Comment Form Inline Errors @ latorante! (Edited Code)
+	*/
+	include dirname(__FILE__) . '/external/comment-form-inline-errors.php';
+	/*
+	 Thanks to Simple Session Support @ Peter Wooster! (Editen Code)
+	*/
+	include dirname(__FILE__) . '/external/simple-session-support.php';
+	
+	
+/*
+********************** ******************** ***********************
+*/
+
+
+// prepare wordpress version for check
+$wp_versions = explode( '.', $wp_version );
+
+add_action('init', 'PlusCaptcha_init', 10);
+function PlusCaptcha_init() {
+  //global $wpdb;
+  wp_enqueue_script('jquery');
+   wp_enqueue_script('jquery-ui-core', false, array('jquery'));
+  wp_enqueue_script('jquery-ui-draggable', false, array('jquery'));
+  wp_enqueue_script('jquery-ui-droppable', false, array('jquery'));
+  wp_enqueue_script( 'plscptf', plugins_url( 'js/plscptf.js', __FILE__ ) );
+}
+
+	// No more PlusCaptcha.php, directly code:
+	
+	// in case this is called like standalone script - we need wordpress functions available
+	if ( ! function_exists( 'get_option' ) ) {
+		// absolute path to wp installation root
+		$wordpress_path = realpath ( dirname ( __FILE__ ) . '/../../../' );
+		require_once $wordpress_path . '/wp-load.php';
+	}
+	
+	session_start();
+	
+	
+wp_enqueue_style( 'PlusCaptcha_Stylesheet', plugins_url( 'css/style.css', __FILE__ ) ); // TODO
+// split action to admin and public part
+if (is_admin()) {
+	require_once PlusCaptcha_LIBRARY . '/admin.php';
+	// Add admin notices.
+	add_action('admin_menu', 'PlusCaptcha_admin_menu');
+	
+	// because various problems with register activation hook trough Wordpress versions - check if Sweet Captcha is installed, otherwise set default values
+	add_action('admin_menu', 'PlusCaptcha_activate');
+	add_action('admin_menu', 'Generate_New_Account');
+	
+	// add uninstall hook -> remove option values - only for wordpress version >= 2.7
+	if ( ( $wp_versions[ 0 ] >=2 ) && ( $wp_versions[ 1 ] >= 7 ) ) {
+		register_uninstall_hook( __FILE__, 'PlusCaptcha_uninstall' );
+	}
+  
+} else {
+	require_once PlusCaptcha_LIBRARY . '/public.php';
+	// add jquery to all public pages
+	//add_action( 'login_head' , 'PlusCaptcha_login_head' );
+  //wp_enqueue_script( 'jquery' );
+
+  // add Sweet Captcha to comment form
+	if ( get_option( 'PlusCaptcha_form_comment' ) ) {
+		add_action( 'comment_form', 'PlusCaptcha_comment_form', 1 );
+		add_filter( 'preprocess_comment', 'PlusCaptcha_comment_form_check', 1 );
+	}
+	
+	// add Sweet Captcha to login form
+	if ( get_option( 'PlusCaptcha_form_login' ) ) {
+    // only for version >= 2.8
+		if ( ( ( $wp_versions[ 0 ] >= 2 ) || ( $wp_versions[ 1 ] > 7 ) ) ) {
+			add_action( 'login_form', 'PlusCaptcha_login_form', 1 );
+			add_filter( 'authenticate', 'PlusCaptcha_authenticate', 40, 3 );
+		}
+    // add PlusCaptcha to BuddyPress login form
+    add_action( 'bp_sidebar_login_form', 'PlusCaptcha_login_form' );
+	}
+	
+	// add Sweet Captcha to lost password form
+	if ( get_option( 'PlusCaptcha_form_lost' ) ) {
+		add_action( 'lostpassword_form', 'PlusCaptcha_login_form', 1 );
+		add_filter( 'allow_password_reset', 'PlusCaptcha_lost_password_check', 1 );
+	}
+	
+}
+
+// PlusCaptcha Contact Form 
+if ( get_option( 'PlusCaptcha_form_contact' ) ) {
+  require_once 'library/shortcode-and-others.php';
+}
---
+++ readme.txt
@@ -0,0 +1,185 @@
+=== PlusCaptcha ===
+Contributors: PlusCaptcha
+Version: 2.0.2
+Tags: pluscaptcha, captcha, antispam, comment, catcha, registration, match captcha, lost password, login, spam, images captcha, capcha, captha, ecology, ecoplus, plus, buddypress, contact form 7, sweetcaptcha, text captcha, multilingual, register, admin, ajax, image, images, keycaptcha, plugin, post, akismet, wpmu, re captcha, recaptcha, wordpress captcha, picture captcha
+Requires at least: 3.0.0
+Tested up to: 3.9.1
+Stable tag: trunk
+
+PlusCaptcha - No more Spam, Easy Set-Up, you can Personalise it. Plus it's FREE!
+
+== Description ==
+
+The first dinamic captcha provider. **Art to your visitors, simple tool to you!**
+
+An evolution of the protection from bots and people without good intentions. Simple to install, simple to use, secure and at the same time help with **EcoPlus**, the biggest ecology project.
+
+= Features =
+
+June 21th **Mega Update!**:
+* New Easy to Read Dashboard!
+* Meet and Discover other Bloggers (and promote your)
+* Cleanest Interface
+* Cleanest Captcha
+
+* Simple to setup.
+* **NO MORE** maths, read words, colors, etc!
+* Automatic account generation **(NEW!)**.
+* Size, color, difficulty personalization.
+* No instructions needed to setup and operate **(NEW!)**.
+* Low bounce percentage.
+* Weekly upgrades.
+
+= Popular Browsers Support =
+
+* **IE** - IE6+
+* **Firefox** - All versions
+* **Chrome** - All versions
+* **Safari** - All versions
+* **Opera** - All versions
+* **Android (NEW!)** - All versions
+* **Mobile Safari (NEW!)** - All versions
+
+= Other Browsers Support (NEW!) =
+
+* AOL
+* **BlackBerry (NEW!)**
+* Camino
+* Dillo
+* Konqueror
+* Lynx
+* Mozilla
+* Netscape
+* Seamonkey
+* Shiira
+
+= Contact Options =
+
+* **General Info / Questions** - info[at]pluscaptcha.com
+* **Support / Bug** - support[at]pluscaptcha.com
+* **Emergency (24/7)** - emergency[at]pluscaptcha.com
+
+= Now you be part of Thousand Webmasters and Bloggers who Promote Ecology! =
+
+PlusCaptcha apart of be a CAPTCHA provider it's an conscientization tool. It's the biggest tool of concientization of the world. Imagine this: every webmaster when setup PlusCaptcha is helping with **EcoPlus** (name of the ecology project), every person who see the nature image becomes aware about the little and fragile world where we live.
+
+**Now, do you can see the Powerfull Tool what is PlusCaptcha?**
+
+= Because it's Simple from Every Point of View =
+
+Simple to install and use, and the most simple way to help the ecology.
+**Just download, activate and it's ready to use** but, if you wanna you can too personalize your account with: **color, size, difficulty and other options!**
+
+= Because is One Solution For All Devices =
+
+1. HTML5, to devices who support it.
+2. JQuery (Javascript), for a little percent of old browsers.
+3. Mail Validation, to all other browsers like of low-cost phones.
+
+= Because is Fully Integrated with WordPress =
+
+1. Integrated Form (with shortcode)
+2. Comment form
+3. Login form
+4. Lost password form
+
+= Because is the Evolution of the old and insecure CAPTCHAS. =
+
+The **"blind strip" (unique technology from PlusCaptcha)** able us to provide the unique CAPTCHA immunity from bots.
+
+= Because is Free by 10 Years =
+
+When your free period expire in 10 years you will able to pay as you want, like a donation. **If not, no problem!** We will provide the same amazing service to you like the first day.
+
+= Because we have two big targets =
+
+1. Work together with your help to **save the ecology**.
+2. Be the most secure and armony CAPTCHA provider of the world.
+
+= Hey! You can help too! =
+
+Help to us translating our website and able to share the oportunity to setup PlusCaptcha in another countries and thats will help to us with our ecology project and meet new developers and communities. Just contact us to: support[at]pluscaptcha.com
+
+== Installation ==
+
+1. Download the PlusCaptcha plugin or install it directly from your dashboard (just search "PlusCaptcha").
+
+2. Install and activate the plugin.
+
+3. Go to PlusCaptcha settings (under "Settings").
+
+At this point, **your account was created automatically (NEW FEATURE!) and you don't need to do more!**, if not, you can do it with next steps:
+
+4. Create a New Account at www.pluscaptcha.com
+
+5. Paste the UUID (code by 9 characters).
+
+6. Save the Settings and Enjoy!
+
+**Remember (NEW FEATURE!): if your account was created automatically, you can personalize it with the user and password provided in the PlusCaptcha Settings Control Panel.**
+
+== Screenshots ==
+
+1. PlusCaptcha Dashboard Settings.
+2. PlusCaptcha in action!
+3. Customize your captcha!
+4. UUID Code Page!
+5. Advance Contact Form Setup
+
+== Requirements ==
+
+* Wordpress more than 2.8
+* Outside connection hosting by fopen().
+
+== Changelog ==
+
+= 2.0.2 =
+* Provided more margin and change structure of comment form.
+* Fixed color letters in comment form.
+
+= 2.0.1 =
+* Fixed mistake provided by feedback to emergency@pluscaptcha.com (thank you!)
+
+= 2.0.0 =
+* New Easy to Read Dashboard!
+* Meet and Discover other Bloggers (and promote your)
+* Cleanest Interface
+* Cleanest Captcha
+
+= 1.2.1 =
+* Default Hided Link
+
+= 1.2.0 =
+* Little Plugin Size Reduction.
+
+= 1.1.2 =
+* New: News section was added.
+
+= 1.1.1 =
+* New: List of Browsers Supported and New Features.
+* Edited information about the plugin.
+
+= 1.1 =
+* New: System to automatically PlusCaptcha account creation.
+* Fix: UTF-8 bug.
+
+= 1.0 =
+* Release version.
+* Do not forget feedback to support[at]pluscaptcha.com.
+
+== Upgrade Notice ==
+
+= 1.2.1 =
+Default Hide Backlin to Maintaining Good Aesthetics
+
+= 1.2.0 =
+Little Plugin Size Reduction.
+
+= 1.1.2 =
+News section was added.
+
+= 1.1.1 =
+List of browsers supported and new features.
+
+= 1.1 =
+Important bug fixed.
